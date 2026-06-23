88ec15293d9e3ef3d687743e978f0df00c6b6da2
shivaniishinde <shivaniishinde96@gmail.com>
2026-06-23 12:03:59 +0530

    Phase 1 - Project Setup Completed

---
+++ digital-healthcare-system/src/main/java/com/shivani/digitalhealthcare/DigitalHealthcareSystemApplication.java
@@ -0,0 +1,14 @@
+package com.shivani.digitalhealthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalHealthcareSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalHealthcareSystemApplication.class, args);
+		System.out.println("hii");
+	}
+
+}
---
+++ digital-healthcare-system/src/test/java/com/shivani/digitalhealthcare/DigitalHealthcareSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivani.digitalhealthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalHealthcareSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
