d0ca310d610fd20bd26ceca9e193ca5dbf652ebc
shivaniishinde <shivaniishinde96@gmail.com>
2026-07-04 13:38:27 +0530

    Implemented BCrypt password encryption

--- digital-healthcare-system/src/main/java/com/shivani/digitalhealthcare/config/SecurityConfig.java
+++ digital-healthcare-system/src/main/java/com/shivani/digitalhealthcare/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.shivani.digitalhealthcare.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,9 +17,15 @@ public class SecurityConfig {
     	  http
           .csrf(csrf -> csrf.disable())
           .authorizeHttpRequests(auth -> auth
-              .anyRequest().authenticated()
-          );
+                  .requestMatchers("/users").permitAll()
+                   .anyRequest().authenticated()
+                   );
 
         return http.build();
     }
-}
+        
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+    }
